41060c09459192071c0e4667dbbb8e0ed0268edf
Ramtin Javanmardi <ramme95@gmail.com>
2024-10-10 19:48:03 +0200

    basic parsing building blocks done, have to add error handling & sync

--- src/parser.py
+++ src/parser.py
@@ -20,3 +20,78 @@ class Parser:
             expression = expr.Binary(expression, operator, right)
 
         return expression
+
+    def comparison(self) -> expr.Expr:
+        expression = self.term()
+
+        while self.match(TokenType.GREATER, TokenType.GREATER_EQUAL, TokenType.LESS, TokenType.LESS_EQUAL):
+            operator = self.previous()
+            right = self.term()
+            expression = expr.Binary(expression, operator, right)
+        return expression
+
+    def term(self) -> expr.Expr:
+        expression = self.factor()
+
+        while self.match(TokenType.MINUS, TokenType.PLUS):
+            operator = self.previous()
+            right = self.factor()
+            expression = expr.Binary(expression, operator, right)
+        return expression
+
+    def factor(self) -> expr.Expr:
+        expression = self.unary()
+
+        while self.match(TokenType.SLASH, TokenType.STAR):
+            operator = self.previous()
+            right = self.unary()
+            expression = expr.Binary(expression, operator, right)
+        return expression
+
+    def unary(self) -> expr.Expr:
+        if self.match(TokenType.BANG, TokenType.MINUS):
+            operator = self.previous()
+            right = self.unary()
+            return expr.Unary(operator, right)
+        return self.primary()
+
+    def primary(self) -> expr.Expr:
+        if self.match(
+            TokenType.FALSE,
+            TokenType.TRUE,
+            TokenType.NIL,
+            TokenType.STRING,
+            TokenType.NUMBER
+        ):
+            return expr.Literal(self.previous().literal)
+
+        if self.match(TokenType.LEFT_PAREN):
+            expression = self.expressions()
+            self.consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.")
+            return expr.Grouping(expression)
+
+    def match(self, *types: TokenType) -> bool:
+        for type in types:
+            if self.check(type):
+                self.advance()
+                return True
+        return False
+
+    def check(self, type: TokenType) -> bool:
+        if self.is_at_end():
+            return False
+        return self.peek().type == type
+
+    def advance(self) -> Token:
+        if self.is_at_end():
+            self.current += 1
+        return self.previous()
+
+    def is_at_end(self) -> bool:
+        return self.peek().type == TokenType.EOF
+
+    def peek(self) -> Token:
+        return self.tokens[self.current]
+
+    def previous(self) -> Token:
+        return self.tokens[self.current - 1]
